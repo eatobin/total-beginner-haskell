52cbaec79f77f0364d07bb7ecfa9fc9c1038ebaf
Eric Tobin <eatobin@gmail.com>
2016-04-29 16:44:38 -0500

    back to [Book]

--- src/Library.hs
+++ src/Library.hs
@@ -82,7 +82,7 @@ getBooksForPerson p bs = [b | b <- bs, getBorrower b == Just p]
 --             newBook = setBorrower (Just p) b
 --             fewerBooks = removeBook b bs
 
-checkOut :: Name -> Title -> [Person] -> Books -> Books
+checkOut :: Name -> Title -> [Person] -> ([Book], Bool) -> ([Book], Bool)
 checkOut n t ps bsb =
   if notMaxedOut && bookNotOut
     then (addBook newBook fewerBooks, True)
