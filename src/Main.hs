5ca7ea442a595345b15fa996d79487005d3e3168
Eric Tobin <eatobin@gmail.com>
2016-05-11 16:16:00 -0500

    all done again

--- src/Main.hs
+++ src/Main.hs
@@ -21,47 +21,46 @@ main = do
   tvBooks <- atomically (newTVar ([], True))
   atomically $ modifyTVar tvBorrowers (addBorrower (makeBorrower "Jim" 3))
   atomically $ modifyTVar tvBorrowers (addBorrower (makeBorrower "Sue" 3))
-  appV (addBook (makeBook "Great Expectations" "Dickens" Nothing)) tvBooks
   atomically $ modifyTVar tvBooks (addBook (makeBook "War And Peace" "Tolstoy" Nothing))
-  --atomically $ modifyTVar tvBooks (addBook (makeBook "Great Expectations" "Dickens" Nothing))
-  books <- atomRead tvBooks
-  borrowers <- atomRead tvBorrowers
-  putStrLn ""
-  putStrLn "Just created new library"
+  atomically $ modifyTVar tvBooks (addBook (makeBook "Great Expectations" "Dickens" Nothing))
+  putStrLn "\nJust created new library"
   printStatus tvBooks tvBorrowers
   putStrLn "Check out War And Peace to Sue"
+  borrowers <- atomRead tvBorrowers
   atomically $ modifyTVar tvBooks (checkOut "Sue" "War And Peace" borrowers)
   printStatus tvBooks tvBorrowers
   putStrLn "Now check in War And Peace from Sue..."
   atomically $ modifyTVar tvBooks (checkIn "War And Peace")
   putStrLn "...and check out Great Expectations to Jim"
+  borrowers <- atomRead tvBorrowers
   atomically $ modifyTVar tvBooks (checkOut "Jim" "Great Expectations" borrowers)
   printStatus tvBooks tvBorrowers
   putStrLn "Add Eric and The Cat In The Hat"
   atomically $ modifyTVar tvBorrowers (addBorrower (makeBorrower "Eric" 1))
   atomically $ modifyTVar tvBooks (addBook (makeBook "The Cat In The Hat" "Dr. Seuss" Nothing))
   putStrLn "Check Out Dr. Seuss to Eric"
+  borrowers <- atomRead tvBorrowers
   atomically $ modifyTVar tvBooks (checkOut "Eric" "The Cat In The Hat" borrowers)
   printStatus tvBooks tvBorrowers
   putStrLn "Now let's do some BAD stuff..."
   putStrLn "Add a borrower that already exists (makeBorrower 'Jim' 3):"
   atomically $ modifyTVar tvBorrowers (addBorrower (makeBorrower "Jim" 3))
   printStatus tvBooks tvBorrowers
-  atomically $ writeTVar tvBorrowers (fst borrowers, True)
+  resetV tvBooks tvBorrowers
   putStrLn "Add a book that already exists (makeBook 'War And Peace' 'Tolstoy' Nothing):"
   atomically $ modifyTVar tvBooks (addBook (makeBook "War And Peace" "Tolstoy" Nothing))
   printStatus tvBooks tvBorrowers
-  --atomically $ writeTVar tvBooks (fst books, True)
   resetV tvBooks tvBorrowers
   putStrLn "Reset! --- All reset?..."
   printStatus tvBooks tvBorrowers
   putStrLn "Check out a valid book to an invalid person (checkOut 'JoJo' 'War And Peace' borrowers):"
+  borrowers <- atomRead tvBorrowers
   atomically $ modifyTVar tvBooks (checkOut "JoJo" "War And Peace" borrowers)
   printStatus tvBooks tvBorrowers
   putStrLn "Check out an invalid book to an valid person (checkOut 'Sue' 'Not A Book' borrowers):"
+  borrowers <- atomRead tvBorrowers
   atomically $ modifyTVar tvBooks (checkOut "Sue" "Not A Book" borrowers)
   printStatus tvBooks tvBorrowers
-  --atomically $ writeTVar tvBooks (fst books, True)
   resetV tvBooks tvBorrowers
   putStrLn "Reset! --- All reset?..."
   printStatus tvBooks tvBorrowers
@@ -77,7 +76,6 @@ printStatus tvbksb tvbrsb = do
   brsb <- atomRead tvbrsb
   if (snd bksb) && (snd brsb) then putStrLn (statusToString bksb brsb)
     else putStrLn "\n*** There was an error with the operation just performed! ***\n"
-appV fn x = atomically $ readTVar x >>= writeTVar x . fn
 resetV tvbksb tvbrsb = do
   bksb <- atomRead tvbksb
   brsb <- atomRead tvbrsb
