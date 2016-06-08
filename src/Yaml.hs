82ef4d367ff5c6e1f65ab126616a469e9ae16492
Eric Tobin <eatobin@gmail.com>
2016-06-08 18:00:12 -0500

    broken

--- src/Yaml.hs
+++ src/Yaml.hs
@@ -8,7 +8,6 @@ import Data.Maybe
 
 import           Book
 import           Borrower
-import Library
 
 import qualified Data.ByteString.Char8 as BS
 
@@ -19,8 +18,19 @@ instance FromJSON Borrower where
     -- A non-Object value is of the wrong type, so fail.
     parseJSON _ = error "Can't parse Borrower from YAML/JSON"
 
+instance FromJSON Book where
+    parseJSON (Object v) = Book <$>
+                           v .: "title" <*>
+                           v .: "author" <*>
+                           v .: "borrower"
+    -- A non-Object value is of the wrong type, so fail.
+    parseJSON _ = error "Can't parse Book from YAML/JSON"
+
 mainly = do
          ymlData <- BS.readFile "borrowers-before.yml"
+         ymlData2 <- BS.readFile "books-before.yml"
          let borrowers = Data.Yaml.decode ymlData :: Maybe [Borrower]
+             books = Data.Yaml.decode ymlData2 :: Maybe [Book]
          -- Print it, just for show
          print $ fromJust borrowers
+         print $ fromJust books
