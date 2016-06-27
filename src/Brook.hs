2842d2a8f2cfdf9671d1b93a75f6f7c6f5a43dbf
Eric Tobin <eatobin@gmail.com>
2016-06-27 13:59:19 -0500

    Brooks progress

--- src/Brook.hs
+++ src/Brook.hs
@@ -7,22 +7,59 @@ type MaxBooks = Int
 type Title = String
 type Author = String
 
-data Brook = Borrower
+data Brook = Br
             { name     :: Name
             , maxBooks :: MaxBooks
             }
-          | Book
+          | Bk
             { title    :: Title
             , author   :: Author
             , borrower :: Maybe Brook
             } deriving (Show, Eq)
 
-makeBrorrower :: Name -> MaxBooks -> Brook
-makeBrorrower = Borrower
+type Brooks = ([Brook], Bool)
 
-makeBrook :: Title -> Author -> Maybe Brook -> Brook
-makeBrook = Book
+makeBr :: Name -> MaxBooks -> Brook
+makeBr = Br
 
-getNamer :: Brook -> Maybe Name
-getNamer Borrower {name} = Just name
-getNamer _ = Nothing
+makeBk :: Title -> Author -> Maybe Brook -> Brook
+makeBk = Bk
+
+getBrName :: Brook -> Maybe Name
+getBrName Br {name} = Just name
+getBrName _ = Nothing
+
+addBrk :: Brook -> Brooks -> Brooks
+addBrk brk brksb =
+  if null coll
+    then (brks ++ [brk], True)
+    else (brks, False)
+      where brks = fst brksb
+            coll = filter (== brk) brks
+
+borrower1 = Br {name = "Borrower1", maxBooks = 1}
+borrower2 = Br {name = "Borrower2", maxBooks = 2}
+borrower3 = Br {name = "Borrower3", maxBooks = 3}
+
+borrowers1 = ([borrower1, borrower2], True)
+borrowers2 = ([borrower1, borrower2, borrower3], True)
+borrowers3 = ([borrower1, borrower2, borrower3], False)
+
+book1 = Bk { title = "Title1"
+           , author = "Author1"
+           , borrower = Just borrower1 }
+book2 = Bk { title = "Title2"
+           , author = "Author2"
+           , borrower = Nothing }
+book3 = Bk { title = "Title3"
+           , author = "Author3"
+           , borrower = Just borrower3 }
+book4 = Bk { title = "Title4"
+           , author = "Author4"
+           , borrower = Just borrower3 }
+
+books1 = ([book1, book2], True)
+books2 = ([book1, book2, book3], True)
+books3 = ([book1, book2, book3, book4], True)
+books4 = ([book1, book2, book3, book4], False)
+books5 = ([book1, book2, book3], False)
