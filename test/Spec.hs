4467155f406543fac1b8fb0f66ef96b7f82d307c
Eric Tobin <eatobin@gmail.com>
2017-04-11 22:59:52 -0500

    Linted!

--- test/Spec.hs
+++ test/Spec.hs
@@ -20,29 +20,29 @@ bk2 = Book { title = "Title2"
 main :: IO ()
 main = hspec $ do
   describe "Borrower tests" $ do
-    it "testMakeBorrower" $ do
-      (makeBorrower "Borrower1" 1) `shouldBe` br1
+    it "testMakeBorrower" $
+      makeBorrower "Borrower1" 1 `shouldBe` br1
 
-    it "testGetName" $ do
-      (getName br1) `shouldBe` "Borrower1"
+    it "testGetName" $
+      getName br1 `shouldBe` "Borrower1"
 
-    it "testSetName" $ do
-      (setName "Borrower1" (Borrower "Jack" 1)) `shouldBe` br1
+    it "testSetName" $
+      setName "Borrower1" (Borrower "Jack" 1) `shouldBe` br1
 
-    it "testGetMaxBooks" $ do
-      (getMaxBooks br1) `shouldBe` 1
+    it "testGetMaxBooks" $
+      getMaxBooks br1 `shouldBe` 1
 
-    it "testSetMaxBooks" $ do
-      (setMaxBooks 11 br1)
+    it "testSetMaxBooks" $
+      setMaxBooks 11 br1
         `shouldBe` Borrower {name = "Borrower1", maxBooks = 11}
 
-    it "testBorrowerToString" $ do
-      (borrowerToString br1) `shouldBe` "Borrower1 (1 books)"
+    it "testBorrowerToString" $
+      borrowerToString br1 `shouldBe` "Borrower1 (1 books)"
 
   describe "Book tests" $ do
-    it "testMakeBookNothing" $ do
-      (makeBook "Title2" "Author2" Nothing) `shouldBe` bk2
+    it "testMakeBookNothing" $
+      makeBook "Title2" "Author2" Nothing `shouldBe` bk2
 
-    it "testMakeBookSomeone" $ do
-      (makeBook "Title1" "Author1" (Just (Borrower "Borrower1" 1)))
+    it "testMakeBookSomeone" $
+      makeBook "Title1" "Author1" (Just (Borrower "Borrower1" 1))
         `shouldBe` bk1
