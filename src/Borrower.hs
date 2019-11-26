51d705ea953af83e050974e85c3b42968be335e1
Eric Tobin <eatobin@gmail.com>
2019-11-26 15:18:32 -0600

    vim format

--- src/Borrower.hs
+++ src/Borrower.hs
@@ -1,40 +1,42 @@
 {-# OPTIONS -Wall #-}
-{-# LANGUAGE NamedFieldPuns    #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Borrower where
 
-import           Data.Aeson
+import Data.Aeson
 
 -- br = Borrower
-
 type Name = String
+
 type MaxBooks = Int
 
-data Borrower = Borrower
-  { name     :: Name
-  , maxBooks :: MaxBooks
-  } deriving (Show, Eq)
+data Borrower =
+  Borrower
+    { name :: Name
+    , maxBooks :: MaxBooks
+    }
+  deriving (Show, Eq)
 
 instance FromJSON Borrower where
   parseJSON (Object v) = Borrower <$> v .: "name" <*> v .: "maxBooks"
-  parseJSON _          = error "Can't parse Borrower from JSON"
+  parseJSON _ = error "Can't parse Borrower from JSON"
 
 instance ToJSON Borrower where
   toJSON (Borrower name maxBooks) =
     object ["name" .= name, "maxBooks" .= maxBooks]
 
 getName :: Borrower -> Name
-getName Borrower { name } = name
+getName Borrower {name} = name
 
 setName :: Name -> Borrower -> Borrower
-setName n br = br { name = n }
+setName n br = br {name = n}
 
 getMaxBooks :: Borrower -> MaxBooks
-getMaxBooks Borrower { maxBooks } = maxBooks
+getMaxBooks Borrower {maxBooks} = maxBooks
 
 setMaxBooks :: MaxBooks -> Borrower -> Borrower
-setMaxBooks m br = br { maxBooks = m }
+setMaxBooks m br = br {maxBooks = m}
 
 borrowerToString :: Borrower -> String
 borrowerToString br = getName br ++ " (" ++ show (getMaxBooks br) ++ " books)"
